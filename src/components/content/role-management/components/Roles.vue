c9f9d9959abffc71406021b64253a89d0f80faa1
hengvisal99 <hengvisal350@gmail.com>
2024-02-17 12:11:28 +0700

    improve role list

--- src/components/content/role-management/components/Roles.vue
+++ src/components/content/role-management/components/Roles.vue
@@ -2,54 +2,34 @@
     <div class="card">
         <DataTable v-model:filters="filters" v-model:selection="selectedCustomer" :value="customers" stateStorage="session"
             stateKey="dt-state-demo-session" paginator :rows="5" filterDisplay="menu" selectionMode="single" dataKey="id"
-            paginatorTemplate="FirstPageLink  PrevPageLink PageLinks  NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[5,10,25]"
-            :pageLinkSize = "3"
+            paginatorTemplate="FirstPageLink  PrevPageLink PageLinks  NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            :rowsPerPageOptions="[5, 10, 25]" :pageLinkSize="3" removableSort
             currentPageReportTemplate="Showing {first} to {last} of {totalRecords} roles"
             :globalFilterFields="['name', 'country.name', 'representative.name', 'status']" tableStyle="min-width: 50rem">
             <template #header>
-                <IconField iconPosition="left">
-                    <InputText v-model="filters['global'].value" placeholder="Search Role" >
-                    </InputText>
-                </IconField>
+
+                <div class="flex justify-between items-center">
+                    <h3 class="text-center">Roles List</h3>
+                    <div>
+                        <InputText class="mr-4" v-model="filters['global'].value" placeholder="Search Role">
+                        </InputText>
+                        <Button label="Add Role"
+                            class="w-44 active:bg-primary-700 hover:bg-primary-800 focus:outline-none bg-primary border-primary " />
+                    </div>
+                </div>
+
             </template>
             <Column field="name" header="ID" sortable style="width: 25%">
-                <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter"
-                        placeholder="Search by name" />
-                </template>
+
             </Column>
-            <Column header="Name" sortable sortField="country.name" filterField="country.name" filterMatchMode="contains"
-                style="width: 25%">
-                <template #body="{ data }">
-                    <div class="flex align-items-center gap-2">
-                        <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                            :class="`flag flag-${data.country.code}`" style="width: 24px" />
-                        <span>{{ data.country.name }}</span>
-                    </div>
-                </template>
-                <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter"
-                        placeholder="Search by country" />
-                </template>
+            <Column field="country.name" header="Name" sortable style="width: 25%">
+
             </Column>
-            <Column header="Action" sortable sortField="representative.name" filterField="representative"
-                :showFilterMatchModes="false" :filterMenuStyle="{ width: '14rem' }" style="width: 25%">
+            <Column header="Action" sortable style="width: 25%">
                 <template #body="{ data }">
-                    <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(data)" />
+                    <Button icon="pi pi-pencil" outlined rounded class="mr-2 text-primary border-primary-200"
+                        @click="editProduct(data)" />
                     <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(data)" />
-                </template>
-                <template #filter="{ filterModel }">
-                    <MultiSelect v-model="filterModel.value" :options="representatives" optionLabel="name" placeholder="Any"
-                        class="p-column-filter">
-                        <template #option="slotProps">
-                            <div class="flex align-items-center gap-2">
-                                <img :alt="slotProps.option.name"
-                                    :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`"
-                                    style="width: 32px" />
-                                <span>{{ slotProps.option.name }}</span>
-                            </div>
-                        </template>
-                    </MultiSelect>
                 </template>
             </Column>
 
@@ -101,4 +81,8 @@ const confirmDeleteProduct = (prod) => {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.p-button.p-button-outlined {
+    /* border-color: var(--primary-color) */
+}
+</style>
